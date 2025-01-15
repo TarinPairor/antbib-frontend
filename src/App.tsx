7242135ccb32f010e2ef4a8a5267b76711f3e394
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-01-15 12:53:17 +0800

    Init clerk

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
 import "./App.css";
 
-function App() {
-  return <div className="bg-red-300">Hi</div>;
+export default function App() {
+  const { user } = useUser();
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+        {user && (
+          <div>
+            <p>Email: {user.primaryEmailAddress?.emailAddress}</p>
+            {/* Add other user data you want to display here */}
+          </div>
+        )}
+      </SignedIn>
+    </header>
+  );
 }
-
-export default App;
